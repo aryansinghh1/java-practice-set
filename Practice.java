5b1f5dce55c0cea31d90e6deab0504e035b8dc05
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-05-25 17:00:56 +0530

    error commit

--- Practice.java
+++ Practice.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
 public class Practice{
     public static void main(String [] args){
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter input: ");
+
+        String str = sc.nextLine();
+
+        char ch;
+
+        for(int i = 0 ; i < str.length(); i++){
+            char current = str.charAt(i);
+
+            if(str.indexOf(current) == -1){
+                ch = current;
+            }
+        }
+
+        System.out.println(ch);
+
+
     }
 }
