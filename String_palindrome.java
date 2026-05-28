5ab2668b557c83f6ac2dafdafb9fbc428e868a0b
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-05-28 15:04:04 +0530

    practice

---
+++ String_palindrome.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class String_palindrome {
+    public static void main(String[] args) {
+        //String palindrome
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your String: ");
+
+        String str = sc.nextLine();
+
+        
+
+        
+
+
+
+    }
+}
