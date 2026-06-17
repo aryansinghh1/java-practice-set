0f26ccb4d116bd603112eb08824535ff8e32cc74
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-06-17 19:20:22 +0530

    Expand_char_with_number

---
+++ Contract_char.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Contract_char {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your stirng: ");
+
+        String str = sc.nextLine();
+
+        
+
+    }
+}
--- Expand_char_with_number.java
+++ Expand_char_with_number.java
@@ -1,7 +1,6 @@
 
 import java.util.Scanner;
 
-
 public class Expand_char_with_number {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -10,22 +9,18 @@ public class Expand_char_with_number {
 
         String str = sc.nextLine();
 
-        
+        for (int i = 0; i < str.length(); i++) {
 
-        for(int i = 0 ; i < str.length(); i++){
-
-            if(Character.isLetter(str.charAt(i))){
+            if (Character.isLetter(str.charAt(i))) {
                 System.out.print(str.charAt(i));
-            }
-            else if(Character.isDigit(str.charAt(i))){
+            } else if (Character.isDigit(str.charAt(i))) {
 
                 int num = Integer.parseInt(String.valueOf(str.charAt(i)));
 
-                for(int j = 0 ; j < num ; j++){
-                    System.out.print(str.charAt(i-1));
+                for (int j = 0; j < num; j++) {
+                    System.out.print(str.charAt(i - 1));
                 }
 
-                
             }
         }
 
