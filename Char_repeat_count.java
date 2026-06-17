f11a3a49113e31d8d03e7eda3ff44ad4fa9eee7a
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-06-18 00:09:33 +0530

    aryan

---
+++ Char_repeat_count.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Char_repeat_count {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your string: ");
+
+        String str = sc.nextLine();
+
+        str = str.toLowerCase();
+
+        // System.out.print(str);
+
+        int[] arr = new int[26];
+
+        for (int i = 0; i < str.length(); i++) {
+            arr[str.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 0) {
+
+                System.out.print((char) (i + 'a') + "" + arr[i]);
+            }
+        }
+
+    }
+}
