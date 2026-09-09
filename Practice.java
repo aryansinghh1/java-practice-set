27aa0527c871ea5d87e5ada389c13a9ceb88eaee
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-09-09 11:02:22 +0530

    Rotate elements by k

--- Practice.java
+++ Practice.java
@@ -1,23 +1,22 @@
 public class Practice {
     public static void main(String[] args) {
 
-        //Rotate elements by k
-        
-        // int[] arr = {1,2,3,4,5};
-        // int k = 0;
-        // int n = 4;
-        
+        // Rotate elements by k
 
-        // while(k<n){
-        //     int temp = arr[k];
-        //     arr[k] = arr[n];
-        //     arr[n] = temp;
-        //     k++;
-        //     n--;
-        // }
+        int[] arr = { 5, 3, 4, 2, 1 };
+        int k = 0;
+        int n = 4;
 
-        // for(int i = 0 ; i < arr.length ; i++){
-        //     System.out.print(arr[i] + " ");
-        // }
+        while(k<n){
+        int temp = arr[k];
+        arr[k] = arr[n];
+        arr[n] = temp;
+        k++;
+        n--;
+        }
+
+        for(int i = 0 ; i < arr.length ; i++){
+        System.out.print(arr[i] + " ");
+        }
     }
 }
